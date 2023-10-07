35a6d6883dc1efcdd281d04b0d9bed2fa91cfba7
Selmen Rayen <selmanerayene95@gmail.com>
2023-10-08 00:58:47 +0100

    integrate prisma & create signup

---
+++ src/user/user.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class UserModule {}
